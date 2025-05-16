13121ae3943ca9e8583f220ba8cfa3a723569a7d
SZN-6 <szn20020812@163.com>
2025-05-16 10:14:42 +0100

    Add files via upload

---
+++ src/data/generated/bib.js
@@ -0,0 +1,105 @@
+const generatedBibEntries = {
+    "chang2024survey": {
+        "author": "Chang, Yupeng and Wang, Xu and Wang, Jindong and Wu, Yuan and Yang, Linyi and Zhu, Kaijie and Chen, Hao and Yi, Xiaoyuan and Wang, Cunxiang and Wang, Yidong and others",
+        "journal": "ACM transactions on intelligent systems and technology",
+        "keywords": "type:survey, Large language models, evaluation, model assessment, benchmark",
+        "number": "3",
+        "pages": "1--45",
+        "publisher": "ACM New York, NY",
+        "series": "ACM TIST",
+        "title": "A survey on evaluation of large language models",
+        "type": "article",
+        "volume": "15",
+        "year": "2024"
+    },
+    "hu2024unveiling": {
+        "author": "Hu, Taojun and Zhou, Xiao-Hua",
+        "journal": "arXiv preprint arXiv:2404.09135",
+        "keywords": "type:evaluation, Large language models, Evaluation, Statistical interpretations, Metrics, Biomedical LLMs, Repositories",
+        "series": "arXiv",
+        "title": "Unveiling llm evaluation focused on metrics: Challenges and solutions",
+        "type": "article",
+        "year": "2024"
+    },
+    "lala2023paperqa": {
+        "author": "L{\\'a}la, Jakub and O'Donoghue, Odhran and Shtedritski, Aleksandar and Cox, Sam and Rodriques, Samuel G and White, Andrew D",
+        "journal": "arXiv preprint arXiv:2312.07559",
+        "keywords": "type:method",
+        "series": "arXiv",
+        "title": "Paperqa: Retrieval-augmented generative agent for scientific research",
+        "type": "article",
+        "year": "2023"
+    },
+    "lewis2020retrieval": {
+        "author": "Lewis, Patrick and Perez, Ethan and Piktus, Aleksandra and Petroni, Fabio and Karpukhin, Vladimir and Goyal, Naman and K{\\\"u}ttler, Heinrich and Lewis, Mike and Yih, Wen-tau and Rockt{\\\"a}schel, Tim and others",
+        "journal": "Advances in neural information processing systems",
+        "pages": "9459--9474",
+        "series": "NeurIPS",
+	"keywords": "type:method",
+        "title": "Retrieval-augmented generation for knowledge-intensive nlp tasks",
+        "type": "article",
+        "volume": "33",
+        "year": "2020"
+    },
+    "li2024scilitllm": {
+        "author": "Li, Sihang and Huang, Jin and Zhuang, Jiaxi and Shi, Yaorui and Cai, Xiaochen and Xu, Mingjun and Wang, Xiang and Zhang, Linfeng and Ke, Guolin and Cai, Hengxing",
+        "journal": "arXiv preprint arXiv:2408.15545",
+        "series": "arXiv",
+	"keywords": "type:application",
+        "title": "Scilitllm: How to adapt llms for scientific literature understanding",
+        "type": "article",
+        "year": "2024"
+    },
+    "liu2025vehicle": {
+        "author": "Liu, Chenxi and Xiao, Zhu and Long, Wangchen and Li, Tong and Jiang, Hongbo and Li, Keqin",
+        "journal": "ACM Computing Surveys",
+        "keywords": "type:survey, Vehicle trajectory, trajectory processing, spatio-temporal data, travel behavior, mobility pattern",
+        "number": "9",
+        "pages": "1--36",
+        "publisher": "ACM New York, NY",
+        "series": "ACM CSUR",
+        "title": "Vehicle Trajectory Data Processing, Analytics, and Applications: A Survey",
+        "type": "article",
+        "volume": "57",
+        "year": "2025"
+    },
+    "peng2025lc": {
+        "author": "Peng, Mingxing and Guo, Xusen and Chen, Xianda and Chen, Kehua and Zhu, Meixin and Chen, Long and Wang, Fei-Yue",
+        "journal": "Communications in Transportation Research",
+        "keywords": "type:application, Lane change, Large language models, Intention prediction, Trajectory prediction, Fine-tuning, Interpretability, Autonomous driving",
+        "pages": "100170",
+        "publisher": "Elsevier",
+        "series": "Commun. Transp. Res",
+        "title": "Lc-llm: Explainable lane-change intention and trajectory predictions with large language models",
+        "type": "article",
+        "volume": "5",
+        "year": "2025"
+    },
+    "sottana2023evaluation": {
+        "author": "Sottana, Andrea and Liang, Bin and Zou, Kai and Yuan, Zheng",
+        "journal": "arXiv preprint arXiv:2310.13800",
+        "series": "arXiv",
+        "title": "Evaluation metrics in the era of GPT-4: Reliably evaluating large language models on sequence to sequence tasks",
+	"keywords": "type:evaluation",
+        "type": "article",
+        "year": "2023"
+    },
+    "wan2024sciqag": {
+        "author": "Wan, Yuwei and Liu, Yixuan and Ajith, Aswathy and Grazian, Clara and Hoex, Bram and Zhang, Wenjie and Kit, Chunyu and Xie, Tong and Foster, Ian",
+        "journal": "arXiv preprint arXiv:2405.09939",
+        "series": "arXiv",
+        "title": "SciQAG: A Framework for Auto-Generated Science Question Answering Dataset with Fine-grained Evaluation",
+	"keywords": "type:evaluation",
+        "type": "article",
+        "year": "2024"
+    },
+    "wei2024diff": {
+        "author": "Wei, Lai and Tan, Zhiquan and Li, Chenghai and Wang, Jindong and Huang, Weiran",
+        "journal": "arXiv preprint arXiv:2401.17139",
+        "series": "arXiv",
+        "title": "Diff-eRank: A Novel Rank-Based Metric for Evaluating Large Language Models",
+        "type": "article",
+	"keywords": "type:evaluation",
+        "year": "2024"
+    }
+};
